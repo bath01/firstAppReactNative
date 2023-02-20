b32b12f1af74c375b3297fa00a742e09d1030622
RUFOS <odetokirufos@gmail.com>
2023-02-20 11:52:31 +0000

    first commit

--- <ScrollView alwaysBounceVertical={false}.js
+++
@@ -1,7 +0,0 @@
-<ScrollView alwaysBounceVertical={false}>
-          {courseGoals.map((goal) => (
-            <View style={styles.goalsItem} key={goal}>
-              <Text style={styles.goalText}>{goal}</Text>
-            </View>
-          ))}
-        </ScrollView>
